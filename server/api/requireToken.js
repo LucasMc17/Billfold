5dccaee721f62d2536b630691171902671fe4d14
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-09 22:46:31 -0500

    fetches deducts on login, can map over them

--- server/api/requireToken.js
+++ server/api/requireToken.js
@@ -5,6 +5,7 @@ const {
 const requireToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
+    console.log(token);
     const user = await User.findByToken(token);
     req.user = user.dataValues;
     next();
