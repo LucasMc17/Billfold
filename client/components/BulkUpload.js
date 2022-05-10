f8fd03291fc671e45dd6c3cff5918c236e7d6d4a
Lucas McGill <lmlucasmc8@gmail.com>
2022-05-10 18:42:29 -0400

    added explanation of bulk upload

--- client/components/BulkUpload.js
+++ client/components/BulkUpload.js
@@ -11,13 +11,9 @@ export default function BulkUpload() {
     state.categories.map((cat) => cat.name)
   );
   const [error, setError] = useState('');
-  const [uploads, setUploads] = useState(0);
-  const [rejects, setRejects] = useState(0);
 
   function handleUpload() {
     const excelUpload = document.querySelector('#excel-upload');
-    setUploads(0);
-    setRejects(0);
     if (!excelUpload.files.length) {
       setError('No file chosen');
     } else if (excelUpload.files[0].name.endsWith('.xls')) {
@@ -32,7 +28,6 @@ export default function BulkUpload() {
             dispatch(postDaily({ category, name, amount, date }));
           } else {
             r++;
-            setRejects(r);
           }
           setError(`${u - r} purchases uploaded. ${r} purchases rejected.`);
         });
@@ -42,7 +37,51 @@ export default function BulkUpload() {
 
   return (
     <div id="bulk-upload">
-      <h1>UPLOAD A FILE TO BILLFOLD</h1>
+      <h1>Upload a File to Billfold</h1>
+      <p>
+        Have a lot of purchases to log? Or maybe you've been tracking your
+        expenses in your own Excel file, and are finally ready to migrate it all
+        to Billfold. Either way, you're in luck! Billfold supports bulk uploads
+        in the form of .xlsx files. Read on to find out how!
+      </p>
+      <h2>Formatting Your Upload</h2>
+      <p>
+        First off, make sure you save your upload as an .xlsx file. Second, the
+        first row of your sheet should be reserved for headers for each of the
+        columns. These headers (in order) will be "Category," "Name," "Amount,"
+        and "Date." In the first column, please enter your purchase's category.
+        In the second, enter the purchase's name. In the third, enter the cost
+        of the purchase. No need to include a dollar sign, just the number will
+        do! Lastly, in the fourth column, enter the date of your purchase. Here
+        is an example of a properly formatted bulk upload:
+      </p>
+      <img src="./billfold-upload-example.PNG" />
+      <p>
+        Note that the date can be formatted as either "MM/DD/YYYY" or
+        "MM/DD/YY."
+      </p>
+      <h2>!!! A Note About Categories !!!</h2>
+      <p>
+        When performing a bulk upload to Billfold, be extra careful to ensure
+        that your categories column is consistent with the names of the
+        categories you've created on Billfold. This includes watching typos and
+        capitalization. For example, if you have a category on Billfold named
+        "laundry", then every deal in your upload in that category must have its
+        category column entered as "laundry", not "Laundry", "laundry ",
+        "landry" or any other permutation. Billfold will not upload a purchase
+        if it cannot locate a corresponding category name, nor will it accept
+        any purchase missing any of the four pieces of information outlined
+        above.
+      </p>
+      <p>
+        After every upload, a message will be displayed informing you how many
+        of the purchases were succesfully uploaded and how many were rejected.
+        To get the rest of the purchases uploaded, take a second look at the
+        category column, and double check for any missing information, before
+        again trying to upload JUST those purchases that did not go through the
+        first time. Uploading the whole sheet again may result in duplicate
+        purchases!
+      </p>
       <input type="file" id="excel-upload" />
       <input type="submit" onClick={handleUpload} />
       <h2>{error}</h2>
