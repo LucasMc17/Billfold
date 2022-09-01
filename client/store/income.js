fe80912bb235f67b616a7da484ce065f081f11c3
Lucas McGill <lmlucasmc8@gmail.com>
2022-09-01 19:24:20 -0400

    cleanup

--- client/store/income.js
+++ client/store/income.js
@@ -2,11 +2,9 @@ import axios from 'axios';
 
 // ACTION TYPES
 const SET_INCOME = 'SET_INCOME';
-const SET_NEW_INCOME = 'SET_NEW_INCOME';
 
 // ACTION CREATORS
 const setIncome = (income) => ({ type: SET_INCOME, income });
-const setNewIncome = (income) => ({ type: SET_NEW_INCOME, income });
 
 // THUNK CREATORS
 export const fetchIncome = (year, month) => {
@@ -23,13 +21,13 @@ export const fetchIncome = (year, month) => {
 
 export const patchIncome = (income) => {
   return async (dispatch) => {
-    dispatch(setIncome(income.income));
     const token = window.localStorage.getItem('token');
     await axios.post(`/api/incomes/new-income`, income, {
       headers: {
         authorization: token,
       },
     });
+    dispatch(setIncome(income.income));
   };
 };
 
@@ -37,8 +35,6 @@ export const patchIncome = (income) => {
 export default function income(state = 40000, action) {
   switch (action.type) {
     case SET_INCOME:
-      return action.income;
-    case SET_NEW_INCOME:
       return action.income;
     default:
       return state;
