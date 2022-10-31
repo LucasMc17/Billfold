ee4e458e590a937844f86449bdf2c6475728deb5
Lucas McGill <lmlucasmc8@gmail.com>
2022-10-30 22:14:18 -0400

    get route now performs a clean up on old ignores

--- server/api/insightIgnores.js
+++ server/api/insightIgnores.js
@@ -4,13 +4,39 @@ const {
 } = require('../db');
 module.exports = router;
 const { requireToken } = require('./requireToken');
+const { Op } = require('sequelize');
+
+function seperateOld(array) {
+  const fresh = [],
+    old = [],
+    now = new Date();
+  for (let ign of array) {
+    const date = new Date(ign.ignoredDate);
+    if (now.getTime() - date.getTime() > 2592000000) {
+      old.push(ign.id);
+    } else {
+      fresh.push(ign);
+    }
+  }
+  return [fresh, old];
+}
 
 router.get('/', requireToken, async (req, res, next) => {
   try {
     const ignores = await InsightIgnore.findAll({
-      userId: req.user.id,
+      where: { userId: req.user.id },
     });
-    res.json(ignores);
+    console.log(ignores);
+    const [toSend, toDel] = seperateOld(ignores);
+    console.log(toSend, toDel);
+    await InsightIgnore.destroy({
+      where: {
+        id: {
+          [Op.in]: toDel,
+        },
+      },
+    });
+    res.json(toSend);
   } catch (err) {
     next(err);
   }
@@ -20,6 +46,7 @@ router.post('/', requireToken, async (req, res, next) => {
   try {
     const ignore = await InsightIgnore.create({
       ...req.body,
+      userId: req.user.id,
     });
     res.json(ignore);
   } catch (err) {
