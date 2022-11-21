101e45f682419668c2049fd914f7e3eb4abfa359
Lucas McGill <lmlucasmc8@gmail.com>
2022-11-20 22:00:12 -0500

    mobile nav functional

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
@@ -7,60 +7,134 @@ import useWindowSize from './custom_hooks/useWindowSize';
 
 const Navbar = ({ handleClick, isLoggedIn, insights }) => {
   const { dynamicWidth } = useWindowSize();
+
+  const [navOpen, toggleNav] = useState(false);
+  console.log(navOpen);
   return (
     <div>
       {isLoggedIn ? (
-        <div id="nav">
-          <Link to="/home">
-            <div id="home-link">
-              <Logo />
-              <h1 id="name">illfold</h1>
-            </div>
-          </Link>
-          <nav>
-            {dynamicWidth > 481 ? (
-              <div id="links">
-                <div>
-                  <Link className="nav-button" to="/myinfo">
-                    My Info
-                  </Link>
-                  <Link className="nav-button" to="/all-months">
-                    All Months
-                  </Link>
-                  <Link className="nav-button" to="/bulk-upload">
-                    Bulk Upload
-                  </Link>
-                  <Link className="nav-button" to="/bulk-export">
-                    Bulk Export
-                  </Link>
-                  <Link
-                    className="nav-button"
-                    to={`/budget-history/${new Date().getFullYear()}`}
-                  >
-                    Budget History
-                  </Link>
-                  <Link className="nav-button insight-button" to="/insights">
-                    Billfold Insights
-                    {insights.length ? (
-                      <div>
-                        <h3>{insights.length}</h3>
-                      </div>
-                    ) : (
-                      <></>
-                    )}
-                  </Link>
-                </div>
-                <div id="logout-button">
-                  <a className="nav-button" href="#" onClick={handleClick}>
-                    Logout
-                  </a>
-                </div>
+        <>
+          <div id="nav">
+            <Link to="/home">
+              <div id="home-link">
+                <Logo />
+                <h1 id="name">illfold</h1>
               </div>
-            ) : (
-              <img id="nav-hamburger" src="/hamburger.png" />
-            )}
-          </nav>
-        </div>
+            </Link>
+            <nav>
+              {dynamicWidth > 481 ? (
+                <div id="links">
+                  <div>
+                    <Link className="nav-button" to="/myinfo">
+                      My Info
+                    </Link>
+                    <Link className="nav-button" to="/all-months">
+                      All Months
+                    </Link>
+                    <Link className="nav-button" to="/bulk-upload">
+                      Bulk Upload
+                    </Link>
+                    <Link className="nav-button" to="/bulk-export">
+                      Bulk Export
+                    </Link>
+                    <Link
+                      className="nav-button"
+                      to={`/budget-history/${new Date().getFullYear()}`}
+                    >
+                      Budget History
+                    </Link>
+                    <Link className="nav-button insight-button" to="/insights">
+                      Billfold Insights
+                      {insights.length ? (
+                        <div>
+                          <h3>{insights.length}</h3>
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                    </Link>
+                  </div>
+                  <div id="logout-button">
+                    <a className="nav-button" href="#" onClick={handleClick}>
+                      Logout
+                    </a>
+                  </div>
+                </div>
+              ) : (
+                <img
+                  id="nav-hamburger"
+                  src="/hamburger.png"
+                  onClick={() => toggleNav(!navOpen)}
+                />
+              )}
+            </nav>
+          </div>
+          {navOpen ? (
+            <div id="mobile-nav">
+              <Link
+                className="mobile-nav-button"
+                to="/myinfo"
+                onClick={() => toggleNav(false)}
+              >
+                My Info
+              </Link>
+              <Link
+                className="mobile-nav-button"
+                to="/all-months"
+                onClick={() => toggleNav(false)}
+              >
+                All Months
+              </Link>
+              <Link
+                className="mobile-nav-button"
+                to="/bulk-upload"
+                onClick={() => toggleNav(false)}
+              >
+                Bulk Upload
+              </Link>
+              <Link
+                className="mobile-nav-button"
+                to="/bulk-export"
+                onClick={() => toggleNav(false)}
+              >
+                Bulk Export
+              </Link>
+              <Link
+                className="mobile-nav-button"
+                to={`/budget-history/${new Date().getFullYear()}`}
+                onClick={() => toggleNav(false)}
+              >
+                Budget History
+              </Link>
+              <Link
+                className="mobile-nav-button insight-button"
+                to="/insights"
+                onClick={() => toggleNav(false)}
+              >
+                Billfold Insights
+                {insights.length ? (
+                  <div>
+                    <h3>{insights.length}</h3>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </Link>
+              <Link
+                className="mobile-nav-button"
+                href="#"
+                onClick={() => {
+                  toggleNav(false);
+                  handleClick();
+                }}
+              >
+                Logout
+              </Link>
+            </div>
+          ) : (
+            <></>
+          )}
+        </>
       ) : (
         ''
       )}
