7d9f1b9e0b99a4fa78184f3065b3465be278201a
Lucas McGill <lmlucasmc8@gmail.com>
2022-11-20 19:12:47 -0500

    hamburger present

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,62 +3,70 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
 import Logo from './Logo';
+import useWindowSize from './custom_hooks/useWindowSize';
 
-const Navbar = ({ handleClick, isLoggedIn, insights }) => (
-  <div>
-    {isLoggedIn ? (
-      <div id="nav">
-        <Link to="/home">
-          <div id="home-link">
-            <Logo />
-            <h1 id="name">illfold</h1>
-          </div>
-        </Link>
-        <nav>
-          <div id="links">
-            <div>
-              <Link className="nav-button" to="/myinfo">
-                My Info
-              </Link>
-              <Link className="nav-button" to="/all-months">
-                All Months
-              </Link>
-              <Link className="nav-button" to="/bulk-upload">
-                Bulk Upload
-              </Link>
-              <Link className="nav-button" to="/bulk-export">
-                Bulk Export
-              </Link>
-              <Link
-                className="nav-button"
-                to={`/budget-history/${new Date().getFullYear()}`}
-              >
-                Budget History
-              </Link>
-              <Link className="nav-button insight-button" to="/insights">
-                Billfold Insights
-                {insights.length ? (
-                  <div>
-                    <h3>{insights.length}</h3>
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </Link>
+const Navbar = ({ handleClick, isLoggedIn, insights }) => {
+  const { dynamicWidth } = useWindowSize();
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div id="nav">
+          <Link to="/home">
+            <div id="home-link">
+              <Logo />
+              <h1 id="name">illfold</h1>
             </div>
-            <div id="logout-button">
-              <a className="nav-button" href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </div>
-          </div>
-        </nav>
-      </div>
-    ) : (
-      ''
-    )}
-  </div>
-);
+          </Link>
+          <nav>
+            {dynamicWidth > 481 ? (
+              <div id="links">
+                <div>
+                  <Link className="nav-button" to="/myinfo">
+                    My Info
+                  </Link>
+                  <Link className="nav-button" to="/all-months">
+                    All Months
+                  </Link>
+                  <Link className="nav-button" to="/bulk-upload">
+                    Bulk Upload
+                  </Link>
+                  <Link className="nav-button" to="/bulk-export">
+                    Bulk Export
+                  </Link>
+                  <Link
+                    className="nav-button"
+                    to={`/budget-history/${new Date().getFullYear()}`}
+                  >
+                    Budget History
+                  </Link>
+                  <Link className="nav-button insight-button" to="/insights">
+                    Billfold Insights
+                    {insights.length ? (
+                      <div>
+                        <h3>{insights.length}</h3>
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                  </Link>
+                </div>
+                <div id="logout-button">
+                  <a className="nav-button" href="#" onClick={handleClick}>
+                    Logout
+                  </a>
+                </div>
+              </div>
+            ) : (
+              <img id="nav-hamburger" src="/hamburger.png" />
+            )}
+          </nav>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
 
 /**
  * CONTAINER
---
+++ client/components/custom_hooks/useWindowSize.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function useWindowSize() {
+  const [screenSize, getDimension] = React.useState({
+    dynamicWidth: window.innerWidth,
+    dynamicHeight: window.innerHeight,
+  });
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+      dynamicHeight: window.innerHeight,
+    });
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('resize', setDimension);
+
+    return () => {
+      window.removeEventListener('resize', setDimension);
+    };
+  }, [screenSize]);
+
+  return screenSize;
+}
