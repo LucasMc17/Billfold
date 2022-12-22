999c433bce027b49ce4406a051efad911ef630f0
Lucas McGill <lmlucasmc8@gmail.com>
2022-12-22 11:19:29 -0500

    linked to personal in footer

--- client/components/SiteFooter.js
+++ client/components/SiteFooter.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function SiteFooter() {
   const isLoggedIn = useSelector((state) => !!state.auth.id);
 
   return isLoggedIn ? (
-    <div id="footer">
-      <h3>
-        <i>Billfold</i>, by Lucas McGill
-      </h3>
-    </div>
+    <Link to={{ pathname: 'https://lucasmcgill.nyc' }} target="_blank">
+      <div id="footer">
+        <h3>
+          <i>Billfold</i>, by Lucas McGill
+        </h3>
+      </div>
+    </Link>
   ) : (
     ''
   );
